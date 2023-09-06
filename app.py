ac6436e5d51229315ed8c89e81a4637b464b593d
Артем Тарбанаков <ezdramiller@gmail.com>
2023-09-06 17:41:13 +0700

    Добавление в .gitignore

--- app.py
+++ app.py
@@ -3,4 +3,24 @@ app= Flask(__name__)
 
 @app.route("/")
 def start(): 
-    return "веб-сервер на flask"
+    return """
+    <!doctype html>
+    <html>
+        <head>
+            <title> Тарбанаков Артем Сергеевич, Лабораторная 1
+        </head>
+        <body>
+            <header> 
+                НГТУ, ФБ, Лабораторная 1
+            </header>
+
+            <h1> веб-сервер на flask </h1>
+
+            <footer>
+                &copy; Тарбанаков Артем Сергеевич, ФБИ-14, 3 курс, 2023
+            </footer>
+         </body>
+    </html>
+    """
+
+        
