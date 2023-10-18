2b4f6555572ca40e36aeb7459fdda74e1fdda24f
Артем Тарбанаков <ezdramiller@gmail.com>
2023-10-18 15:38:17 +0700

    заказ напитка

--- lab3.py
+++ lab3.py
@@ -19,3 +19,48 @@ def form1():
     age = request.args.get('age')
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route("/lab3/order")
+def order():
+    price1=0
+    price2=0
+    drink = request.args.get('drink')
+    if drink == 'coffe':
+        price1 += 120
+    elif drink == 'black-tea':
+        price1 += 80
+    elif drink == 'green-tea':
+        price1 += 70
+
+    if request.args.get('milk') == 'on' and request.args.get('sugar') == 'on':
+        price2 += 40
+    elif request.args.get('milk') == 'on':
+        price2 += 30
+    elif request.args.get('sugar') == 'on':
+        price2 += 10
+
+    return render_template('order.html', price1=price1, price2=price2)
+
+
+@lab3.route("/lab3/pay")
+def pay():
+    price=0
+    drink = request.args.get('drink')
+    if drink == 'coffe':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    return render_template('pay.html', price = price)
+
+
+@lab3.route("/lab3/success")
+def thanks():
+    return render_template('success.html')
