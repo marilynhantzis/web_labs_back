ab23273190335b8e856b9cd427243c90dcdb2907
Артем Тарбанаков <ezdramiller@gmail.com>
2023-10-20 19:13:11 +0700

    авторизация

--- lab4.py
+++ lab4.py
@@ -2,6 +2,23 @@ from flask import Blueprint, redirect, url_for, render_template, request
 lab4 = Blueprint('lab4',__name__)
 
 
-@lab4.route('/lab4')
+@lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods= ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'marilynhantzis' and password == '1337':
+        return render_template('success1.html', username = username)
+    elif username == '' or password == '':
+        error1 = 'Заполните поля!'
+        return render_template('login.html', error1=error1, username = username, password = password)
+    else:
+        error2 = 'Неверные логин и/или пароль'
+        return render_template('login.html', username = username, error2=error2, password = password)
+    
