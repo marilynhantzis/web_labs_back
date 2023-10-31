1638f87cf5a9ed1fcfa654bb1856c32d5917ab7d
Артем Тарбанаков <ezdramiller@gmail.com>
2023-10-31 19:12:25 +0700

    холодильник. часть 2

--- lab4.py
+++ lab4.py
@@ -21,4 +21,42 @@ def login():
     else:
         error2 = 'Неверные логин и/или пароль'
         return render_template('login.html', username = username, error2=error2, password = password)
-    
+
+
+@lab4.route('/lab4/fridge', methods= ['GET', 'POST'])
+def fridge():
+    temp = request.form.get('temp')
+    head = ''
+    foot = ''
+    src=''
+    if request.method == 'GET':
+        return render_template('fridge.html')
+    if temp == '':
+        error = 'Указано пустое значение'
+        return render_template('fridge.html', temp=temp, error = error)
+    elif float(temp) < -12:
+        head = 'Не удалось установить температуру'
+        foot = 'Слишком низкое значение'
+        return render_template('fridge_temp.html', temp=temp, head=head, foot=foot)
+    elif float(temp) > -1:
+        head = 'Не удалось установить температуру'
+        foot = 'Слишком высокое значение'
+        return render_template('fridge_temp.html', temp=temp, head=head, foot=foot)
+    elif -12 <= float(temp) <= -9:
+        head = 'Установлена температура'
+        foot = 'Установлена температура: ' + temp +'°C'
+        src= 'blue_snowflake.jpg'
+        return render_template('fridge_temp.html', temp=temp, head=head, foot=foot, src=src)
+    elif -8 <= float(temp) <= -5:
+        head = 'Установлена температура'
+        foot = 'Установлена температура: ' + temp +'°C'
+        src = 'blue_snowflake.jpg'
+        src2 = 'blue_snowflake.jpg'
+        return render_template('fridge_temp.html', temp=temp, head=head, foot=foot, src=src, src2=src2)
+    elif -4 <= float(temp) <= -1:
+        head = 'Установлена температура'
+        foot = 'Установлена температура: ' + temp +'°C'
+        src= 'blue_snowflake.jpg'
+        src2 = 'blue_snowflake.jpg'
+        src3 = 'blue_snowflake.jpg'
+        return render_template('fridge_temp.html', temp=temp, head=head, foot=foot, src=src, src2=src2, src3=src3)
