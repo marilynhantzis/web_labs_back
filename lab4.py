d5467192549ddb4798144720352760494f077ef2
Артем Тарбанаков <ezdramiller@gmail.com>
2023-11-01 02:08:15 +0700

    заказ зерна

--- lab4.py
+++ lab4.py
@@ -60,3 +60,54 @@ def fridge():
         src2 = 'blue_snowflake.jpg'
         src3 = 'blue_snowflake.jpg'
         return render_template('fridge_temp.html', temp=temp, head=head, foot=foot, src=src, src2=src2, src3=src3)
+    
+@lab4.route('/lab4/zerno', methods= ['GET', 'POST'])
+def zerno(): 
+    weight = request.form.get('weight')
+    zerno = request.form.get('zerno')
+    order = 0
+    sum=0
+    sale=0
+    if request.method == 'GET':
+        return render_template('order_zerno.html')
+    if weight == '':
+        order = 1
+        return render_template('zerno.html', order = order)
+    elif float(weight) > 500:
+        order = 2
+        return render_template('zerno.html', order = order)
+    elif float(weight) <= 0:
+        order = 3
+        return render_template('zerno.html', order = order)
+    elif float(weight) > 50:
+        order = 4 
+        if zerno == 'first':
+            sum= 12000 * float(weight)
+            zerno = 'Ячмень'
+        elif zerno == 'second':
+            sum = 8500 * float(weight)
+            zerno = 'Овес'
+        elif zerno == 'third':
+            sum = 8700 * float(weight)
+            zerno = 'Пшеница'
+        elif zerno == 'four':
+            sum = 14000 * float(weight)
+            zerno = 'Рожь'
+        sale=0.1*sum
+        sum= sum - sale
+        return render_template('zerno.html', order = order, sum=sum, sale=sale, weight=weight, zerno=zerno)
+    else:
+        order = 5 
+        if zerno == 'first':
+            sum= 12000 * float(weight)
+            zerno = 'Ячмень'
+        elif zerno == 'second':
+            sum = 8500 * float(weight)
+            zerno = 'Овес'
+        elif zerno == 'third':
+            sum = 8700 * float(weight)
+            zerno = 'Пшеница'
+        elif zerno == 'four':
+            sum = 14000 * float(weight)
+            zerno = 'Рожь'
+        return render_template('zerno.html', order = order, sum=sum, weight=weight, zerno=zerno)
