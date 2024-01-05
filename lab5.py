36323afebc3ab6ba11d6ecd3f03cfc10b5713165
Артем Тарбанаков <ezdramiller@gmail.com>
2024-01-06 00:23:25 +0700

    регистрация и добавление пользователей

--- lab5.py
+++ lab5.py
@@ -4,48 +4,75 @@ import psycopg2
 
 lab5 = Blueprint('lab5',__name__)
 
-
-@lab5.route('/lab5/')
-def main(): 
+def dbConnect(): 
     conn = psycopg2.connect(
         host="127.0.0.1",
         database="knowledge_base_tarbanakov",
         port="5432",
         user="tarbanakov_knowledge_base",
-        password="123"
-    )
+        password="123")
+    
+    return conn
 
-    cur = conn.cursor()
+def dbClose(cursor, connection): 
+    cursor.close()
+    connection.close()
 
-    cur.execute('SELECT * FROM users;')
 
-    result = cur.fetchall()
 
-    cur.close()
-    conn.close()
 
-    print(result)
+@lab5.route('/lab5/')
+def main(): 
+    visiblename = 'Аноним'
 
-    return 'go to console'
+    return render_template('baza_main.html', visiblename = visiblename)
 
 
 @lab5.route('/lab5/users')
 def main1(): 
-    conn = psycopg2.connect(
-        host="127.0.0.1",
-        database="knowledge_base_tarbanakov",
-        port="5432",
-        user="tarbanakov_knowledge_base",
-        password="123"
-    )
-
+    conn = dbConnect()
     cur = conn.cursor()
 
     cur.execute('SELECT * FROM users;')
 
     result = cur.fetchall()
 
-    cur.close()
-    conn.close()
+    dbClose(cur, conn)
 
     return render_template('result.html', result = result)
+
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+def registerPage():
+    errors=[]
+
+    if request.method == 'GET': 
+        return render_template('register.html', errors=errors)
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not (username or password):
+        errors.append('Пожалуйста, заполните все поля')
+        print(errors)
+        return render_template('register.html', errors=errors)
+    
+    conn = dbConnect()
+    cur = conn.cursor()
+
+
+    cur.execute(f"SELECT username FROM users WHERE username = '{ username}';")
+
+    if cur.fetchone() is not None:
+        errors.append('Пользователь с данным именем уже существует')
+        dbClose(cur, conn)
+        return render_template('register.html', errors=errors)
+    
+    cur.execute(f"INSERT INTO users (username, password) VALUES ('{username}', '{password}');")
+
+    conn.commit()
+    dbClose(cur, conn)
+
+    return redirect('/lab5/')
+
+
