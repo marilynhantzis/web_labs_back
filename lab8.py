130e10d71f80e245750ebaef582f54c413ad95c0
Артем Тарбанаков <ezdramiller@gmail.com>
2024-01-10 21:08:14 +0700

    получение информации о курсах

--- lab8.py
+++ lab8.py
@@ -1,7 +1,18 @@
 from flask import Blueprint, render_template, request, abort
 
-lab7 = Blueprint('lab7', __name__)
+lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8')
 def main():
     return render_template('/lab8/index.html')
+
+courses = [ 
+        {'name': 'c++', 'videos': 3, 'price': 3000},
+        {'name': 'basic', 'videos': 30, 'price': 0},
+        {'name': 'c#', 'videos': 8}
+]
+
+
+@lab8.route('/lab8/api/courses/', methods=["GET"])
+def get_courses():
+    return courses
